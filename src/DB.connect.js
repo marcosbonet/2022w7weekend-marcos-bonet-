619c5f65b032a328990ffa95dc7cb99e4b2300df
marcos <marcos.bonet@live.com.ar>
2022-11-18 19:51:03 +0100

    robot repository controller

--- src/DB.connect.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-import { USER, CLUSTER, PASSW } from './config.js';
-export function dbConnect() {
-    const DBName = 'Players';
-    let uri = `mongodb+srv://${USER}:${PASSW}`;
-    uri += `@${CLUSTER}/${DBName}?retryWrites=true&w=majority`;
-    return mongoose.connect(uri);
-}
--- src/DB.connect.test.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-import { dbConnect } from './DB.connect.js';
-test('should first', async () => {
-    const result = await dbConnect();
-    expect(typeof result).toBe(typeof mongoose);
-    mongoose.disconnect();
-});
