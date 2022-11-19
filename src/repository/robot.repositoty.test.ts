91bc23f794d1bb4b46f4da39eb71da9c1a34cd22
marcos <marcos.bonet@live.com.ar>
2022-11-19 16:02:10 +0100

    test contoller

--- src/repository/robot.repositoty.test.ts
+++ src/repository/robot.repositoty.test.ts
@@ -1,73 +1,76 @@
 import mongoose from 'mongoose';
 import { dbConnect } from '../DB.connect.js';
+
 import { RobotRepository } from './robot.repository.js';
 
-const PlayerMock = [
+const mockData = [
     {
-        robotName: 'string',
-        velocity: 3,
-        resistent: 4,
-        creationDate: 'string',
         id: 'string',
+        robotName: 'string',
+        velocity: 1,
+        resistent: 2,
+        creationDate: 'string',
+
         img: 'string',
     },
     {
-        robotName: 'string',
-        velocity: 3,
-        resistent: 4,
-        creationDate: 'string',
         id: 'string',
+        robotName: 'string',
+        velocity: 1,
+        resistent: 2,
+        creationDate: 'string',
         img: 'string',
     },
 ];
+describe('Given RobotRepository', () => {
+    describe('When we instantiate it', () => {
+        const repository = new RobotRepository();
+        let testIds: Array<string>;
 
-describe('Given ...', () => {
-    const repository = new RobotRepository();
+        beforeAll(async () => {
+            await dbConnect();
+            await repository.getModel().deleteMany();
+            await repository.getModel().insertMany(mockData);
+            const data = await repository.getModel().find();
+            testIds = [data[0].id, data[1].id];
+        });
+        afterAll(() => {
+            mongoose.disconnect();
+        });
 
-    let testIds: Array<string>;
-    beforeAll(async () => {
-        await dbConnect();
-        await repository.getModel().deleteMany();
-        await repository.getModel().insertMany(PlayerMock);
-        const data = await repository.getModel().find();
-        testIds = [data[0].id, data[1].id];
-    });
-
-    test('Then getAll...', async () => {
-        const result = await repository.getAll();
-        expect(result[0].robotName).toEqual(PlayerMock[0].robotName);
-    });
-    test('the get should have been called', async () => {
-        const result = await repository.get(testIds[0]);
-        expect(result.velocity).toBe('string');
-    });
-    test('the post should have been called', async () => {
-        const newRobot = {
-            robotName: 'string',
-            velocity: 3,
-            resistent: 4,
-            creationDate: 'string',
-            id: 2,
-            img: 'string',
-        };
-        const result = await repository.post(newRobot);
-        expect(result).toEqual(newRobot.robotName);
-    });
-    test('the patch should have been called', async () => {
-        const result = await repository.patch('', PlayerMock[0]);
-        expect(result).toEqual(PlayerMock);
-    });
-    test('the delete should have been called', async () => {
-        const result = await repository.delete(testIds[1]);
-        expect(result).toEqual([]);
-    });
-
-    test('Then id is bad forman', async () => {
-        expect(async () => {
-            await repository.delete('');
-        }).rejects.toThrowError(mongoose.Error.CastError);
-    });
-    afterAll(() => {
-        mongoose.disconnect();
+        test('Then getAll should have been called', async () => {
+            const result = await repository.getAll();
+            expect(result[0].img).toEqual(mockData[0].img);
+            expect(await repository.get.all);
+        });
+        test('Then get should have been called', async () => {
+            const result = await repository.get(1);
+            expect(result.img).toEqual('jsd');
+        });
+        test('Then post should have been called', async () => {
+            const newRobot = {
+                id: 'string',
+                robotName: 'string',
+                velocity: 1,
+                resistent: 2,
+                creationDate: 'string',
+                img: 'string',
+            };
+            const result = await repository.post(newRobot);
+            expect(result.velocity).toEqual(newRobot.velocity);
+        });
+        test('Then patch should have been called', async () => {
+            const result = await repository.patch(4, mockData[0]);
+            expect(result).toEqual(mockData);
+        });
+        test('Then delete should have been called', async () => {
+            const result = await repository.delete(2);
+            expect(result).toEqual([]);
+        });
+        test('Then if id is bad formated delete should throw an error', async () => {
+            expect(async () => {
+                await repository.delete(2);
+            }).rejects.toThrowError(mongoose.Error.CastError);
+        });
     });
 });
